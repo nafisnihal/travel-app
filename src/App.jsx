552fd6ed4744848da3c57118743325857832412f
Nafis Nihal <nafisnihal12@gmail.com>
2025-08-17 18:55:21 +0600

    feat: moved main application structure from main to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { ThemeProvider } from "./components/theme-provider";
+import "./index.css";
+import Layout from "./layouts/Layout";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider defaultTheme="system">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { ThemeProvider } from "./components/theme-provider";
+import App from "./App";
 import "./index.css";
-import About from "./pages/About.jsx";
-import Home from "./pages/Home.jsx";
-import Layout from "./layouts/Layout";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system">
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
